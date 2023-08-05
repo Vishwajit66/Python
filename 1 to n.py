91ab3332ad139acaf44845b3332c0aea96acddef
vishwajit deokar <dekarv35@gmail.com>
2023-08-05 20:30:09 +0530

    2nd

--- 1 to n.py
+++ 1 to n.py
@@ -1,6 +1,9 @@
+# print number 1 to 100
 n= int()
 for n in range  (0,101):
     print(n)
+
+# Print number 1 to 100 in the  distans of 10 / print the 10 table 
 
 n= int()
 for n in range  (0,101,10):
